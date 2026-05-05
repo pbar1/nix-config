32ab431d5dcfee74aec58bfde2f2a7678272c5dd
Pierce Bartine <piercebartine@gmail.com>
2026-05-05 16:59:44 -0700

    Remove sshd profiles

--- nixos-tec/apparmor/default.nix
+++ nixos-tec/apparmor/default.nix
@@ -9,23 +9,6 @@
 
   security.apparmor.enable = true;
 
-  security.apparmor.policies.sshd = {
-    state = "complain";
-    profile = import ./sshd.nix;
-  };
-  security.apparmor.policies.sshd-session = {
-    state = "complain";
-    profile = import ./sshd-session.nix;
-  };
-  security.apparmor.policies.sshd-auth = {
-    state = "complain";
-    profile = import ./sshd-auth.nix;
-  };
-  security.apparmor.policies.sftp-server = {
-    state = "complain";
-    profile = import ./sftp-server.nix;
-  };
-
   security.apparmor.policies.plex = {
     state = "complain";
     profile = import ./plex.nix;
