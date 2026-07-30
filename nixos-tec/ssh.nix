99245d6bda006c3e0a4689e947310177ebb50ac8
Pierce Bartine <26949935+pbar1@users.noreply.github.com>
2026-07-29 19:43:13 -0700

    Add iOS ssh keys

--- nixos-tec/ssh.nix
+++ nixos-tec/ssh.nix
@@ -27,5 +27,7 @@
   users.users."nixos".openssh.authorizedKeys.keys = [
     "verify-required sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHxrK0/YVAOMW+IKh7TPw8URtahQEEhcmu2q64i+ckzOAAAABHNzaDo= yubikey"
     "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOdw035S5U0YP971KASp1NFK3BR2vmEflQxzn5ECwY4HEsiCfMQr20bo2JI47M/E6BVK/38AdEcixjESUYNQm7s= bobbery"
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBMaNwRzHuSDEwQXezB9qmmqEujhc+tx923ovtmb6boMlwcAG53z17ofNJshZlnEo3NbEbVmbLMOKglY0TNPDyhA= goombella"
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDrF2mGkbfDEZe22CyngsgbOYw9jZjfsK0eoBUH6zif2jzC66+9dc+T/yubOQkdZ1wMpGHWOCwlKmiylK7R4s0Y= koops"
   ];
 }
